961585c1313ddc556f3398e2db9ca2052814f477
Alex Guerra <alex@heyimalex.com>
2019-02-20 02:37:14 -0600

    Prettier

--- leven/bench.js
+++ leven/bench.js
@@ -14,57 +14,57 @@ var Benchmark = require("benchmark");
 var suite = new Benchmark.Suite();
 
 function run(fn) {
-	fn("a", "b");
-	fn("ab", "ac");
-	fn("ac", "bc");
-	fn("abc", "axc");
-	fn("kitten", "sitting");
-	fn("xabxcdxxefxgx", "1ab2cd34ef5g6");
-	fn("cat", "cow");
-	fn("xabxcdxxefxgx", "abcdefg");
-	fn("javawasneat", "scalaisgreat");
-	fn("example", "samples");
-	fn("sturgeon", "urgently");
-	fn("levenshtein", "frankenstein");
-	fn("distance", "difference");
-	fn("因為我是中國人所以我會說中文", "因為我是英國人所以我會說英文");
+  fn("a", "b");
+  fn("ab", "ac");
+  fn("ac", "bc");
+  fn("abc", "axc");
+  fn("kitten", "sitting");
+  fn("xabxcdxxefxgx", "1ab2cd34ef5g6");
+  fn("cat", "cow");
+  fn("xabxcdxxefxgx", "abcdefg");
+  fn("javawasneat", "scalaisgreat");
+  fn("example", "samples");
+  fn("sturgeon", "urgently");
+  fn("levenshtein", "frankenstein");
+  fn("distance", "difference");
+  fn("因為我是中國人所以我會說中文", "因為我是英國人所以我會說英文");
 }
 
 suite
-	.add("leven", function() {
-		run(leven);
-	})
-	.add("leven_wasm", function() {
-		run(leven_wasm);
-	})
-	.add("talisman", function() {
-		run(talisman);
-	})
-	.add("levenshtein-edit-distance", function() {
-		run(levenshteinEditDistance);
-	})
-	.add("fast-levenshtein", function() {
-		run(fastLevenshtein);
-	})
-	.add("levenshtein-component", function() {
-		run(levenshteinComponent);
-	})
-	.add("levdist", function() {
-		run(levdist);
-	})
-	.add("ld", function() {
-		run(ld);
-	})
-	.add("natural", function() {
-		run(natural);
-	})
-	.add("levenshtein", function() {
-		run(levenshtein);
-	})
-	.on("cycle", function(event) {
-		console.log(String(event.target));
-	})
-	.on("complete", function() {
-		console.log("Fastest is " + this.filter("fastest").map("name"));
-	})
-	.run({ async: true });
+  .add("leven", function() {
+    run(leven);
+  })
+  .add("leven_wasm", function() {
+    run(leven_wasm);
+  })
+  .add("talisman", function() {
+    run(talisman);
+  })
+  .add("levenshtein-edit-distance", function() {
+    run(levenshteinEditDistance);
+  })
+  .add("fast-levenshtein", function() {
+    run(fastLevenshtein);
+  })
+  .add("levenshtein-component", function() {
+    run(levenshteinComponent);
+  })
+  .add("levdist", function() {
+    run(levdist);
+  })
+  .add("ld", function() {
+    run(ld);
+  })
+  .add("natural", function() {
+    run(natural);
+  })
+  .add("levenshtein", function() {
+    run(levenshtein);
+  })
+  .on("cycle", function(event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function() {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  .run({ async: true });
